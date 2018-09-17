24482be65dd9b6ec71bc32c4a77ed50eb42bcd57
Yusra Ozohu <yozohu1@gmail.com>
2018-09-17 14:12:29 -0400

    lab1 stuff

---
+++ PointC.java
@@ -0,0 +1,92 @@
+// This file contains material supporting section 2.9 of the textbook:
+// "Object Oriented Software Engineering" and is issued under the open-source
+// license found at www.lloseng.com 
+
+/**
+ * This class contains instances of coordinates in either polar or
+ * cartesian format.  It also provides the utilities to convert
+ * them into the other type. It is not an optimal design, it is used
+ * only to illustrate some design issues.
+ *
+ * @author Fran&ccedil;ois B&eacute;langer
+ * @author Dr Timothy C. Lethbridge
+ * @version July 2000
+ */
+public class PointC
+{
+  //Instance variables ************************************************
+
+  /**
+   * Contains the current value of X
+   */
+  private double x;
+  
+  /**
+   * Contains the current value of Y
+   */
+  private double y;
+	
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public PointC(double xOrRho, double yOrTheta)
+  {
+    this.x = xOrRho;
+    this.y = yOrTheta;
+  }
+	
+  
+  //Instance methods **************************************************
+ 
+ 
+  public double getX()
+  {
+      return x;
+  }
+  
+  public double getY()
+  {
+      return y;
+  }
+  
+  public double getRho()
+  {
+      return (Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+  }
+  
+  public double getTheta()
+  {
+      return Math.toDegrees(Math.atan2(y, x));
+  }
+  
+  /**
+   * Calculates the distance in between two points using the Pythagorean
+   * theorem  (C ^ 2 = A ^ 2 + B ^ 2). Not needed until E2.30.
+   *
+   * @param pointA The first point.
+   * @param pointB The second point.
+   * @return The distance between the two points.
+   */
+  public double getDistance(PointC pointB)
+  {
+    // Obtain differences in X and Y, sign is not important as these values
+    // will be squared later.
+    double deltaX = getX() - pointB.getX();
+    double deltaY = getY() - pointB.getY();
+    
+    return Math.sqrt((Math.pow(deltaX, 2) + Math.pow(deltaY, 2)));
+  }
+
+  /**
+   * Returns information about the coordinates.
+   *
+   * @return A String containing information about the coordinates.
+   */
+  public String toString()
+  {
+    return "(" + x + ", " + y + ")";
+  }
+}
